ff6049b42b89d1b54d2b76c09cd045700352ab6b
Tim O'Guin <tim.oguin@flexdeaf.com>
2014-06-20 15:21:09 -0500

    added SQLite database, updated models, created db creation script, and added an ipython notebook testing the incomplete CSV loader

--- app/models.py
+++ app/models.py
@@ -4,5 +4,6 @@ class Census(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     state = db.Column(db.String(30), index = True)
     sex = db.Column(db.String(2), index = True)
+    age = db.Column(db.Integer)
     pop2000 = db.Column(db.Integer)
     pop2008 = db.Column(db.Integer)
